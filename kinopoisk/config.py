0980374cab7bab0f4b54800a6d088c8769bb3fbe
Roman Maliushkin <malyushkinr@gmail.com>
2022-10-16 00:03:43 +0200

    Add config file

---
+++ kinopoisk/config.py
@@ -0,0 +1,4 @@
+KP_LIST_SIZE = 50
+KP_URL = "https://www.kinopoisk.ru/{path}"
+KP_USER_LIST_URL = "https://www.kinopoisk.ru/user/{user}/votes/list/vs/vote/page/{page_num}/#list"
+KP_USER_ID = 964636
