fba7facb52931c2f30c20a3e8a152c11ddb393a1
Roman Maliushkin <malyushkinr@gmail.com>
2022-10-22 22:43:25 +0200

    Remove old Kinopoisk folder

--- kinopoisk/config.py
+++
@@ -1,8 +0,0 @@
-KP_LIST_SIZE = 50
-KP_URL = "https://www.kinopoisk.ru{path}"
-KP_USER_LIST_URL = "https://www.kinopoisk.ru/user/{user}/votes/list/vs/vote/page/{page_num}/#list"
-KP_USER_ID = 964636
-
-USER_AGENT_HEADER = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) " \
-                    "AppleWebKit/537.36 (KHTML, like Gecko) " \
-                    "Chrome/106.0.0.0 Safari/537.36"
